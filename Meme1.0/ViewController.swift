5ab8762d65a34377a2adf595b4c41373da530c7c
Eric Hodgins <hodgins.e@gmail.com>
2015-12-05 14:33:51 -0500

    Initial Commit

---
+++ Meme1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme1.0
+//
+//  Created by Eric Hodgins on 2015-12-05.
+//  Copyright © 2015 Eric Hodgins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
